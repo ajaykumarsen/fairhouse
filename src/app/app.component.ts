089748430affa54d4be3f31dc52274a6b8f783d6
ajaykumarsen <ajaykumarsen@protonmail.com>
2022-09-24 22:27:27 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from './housing-location';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'fairhouse';
+
+  housingLocationList: HousingLocation[] = [
+    {
+      name: 'acme fresh start housing',
+      city: 'Chicago',
+      state: 'IL',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      name: 'veer housing',
+      city: 'California',
+      state: 'California',
+      availableUnits: 3,
+      wifi: false,
+      laundry: true,
+    },
+  ];
+
+  selectedLocation: HousingLocation | undefined;
+
+  updateSelectedLocation(location: HousingLocation) {
+    this.selectedLocation = location;
+  }
 }
---
+++ src/app/housing-location.ts
@@ -0,0 +1,8 @@
+export interface HousingLocation {
+  name: string;
+  city: string;
+  state: string;
+  availableUnits: number;
+  wifi: boolean;
+  laundry: boolean;
+}
